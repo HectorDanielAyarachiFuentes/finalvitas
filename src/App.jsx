9fb92e9d41af4fee26da6acd43b014ccfe0ccaff
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-10-12 17:48:28 -0300

    drfg

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
           <img src={viteLogo} className="logo" alt="Logotipo de Vite" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           <img src={reactLogo} className="logo react" alt="Logotipo de React" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React App</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          el contador es {count}
+        <button onClick={incrementCount}>
+          Incrementar contador ({count})
+        </button>
+        <button onClick={resetCount}>
+          Reiniciar contador
         </button>
         <p>
-          Edita <code>src/App.jsx</code> y guarda para probar la Recarga Rápida del Módulo (HMR).
+          Edita el archivo <code>src/App.jsx</code> y guarda para probar la Recarga Rápida del Módulo (HMR).
         </p>
       </div>
       <p className="read-the-docs">
         Haz clic en los logotipos de Vite y React para obtener más información.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
